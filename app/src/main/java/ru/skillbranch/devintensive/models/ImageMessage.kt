06620625aef25ae86950f90e9c4cc1556ded31c1
Anna Shabaeva <av.shabaeva@severmed.com>
2019-09-05 06:50:48 +0300

    fix

--- app/src/main/java/ru/skillbranch/devintensive/models/ImageMessage.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/ImageMessage.kt
@@ -3,13 +3,16 @@ package ru.skillbranch.devintensive.models
 import ru.skillbranch.devintensive.extensions.humanizeDiff
 import java.util.*
 
-class ImageMessage (
-    id: String,
-    from: User?,
-    chat: Chat,
-    isIncoming: Boolean = false,
+
+class ImageMessage(
+    id:String,
+    from:User?,
+    chat:Chat,
+    isIncoming:Boolean = false,
     date: Date = Date(),
-    var image: String?
-) : BaseMessage(id, from, chat, isIncoming, date) {
-    override fun formatMessage(): String = "id:$id ${from?.firstName} ${if(isIncoming) "получила" else "отправила"} изображение \"$image\" ${date.humanizeDiff()}"
+    var image : String?)
+    : BaseMessage(id, from, chat, isIncoming, date) {
+    override fun formatMessage(): String = "id $id ${from?.firstName} " +
+            "${if (isIncoming) "получил" else "отправил"} изображение \"$image\" ${date.humanizeDiff()}"
+
 }
--- app/src/main/java/ru/skillbranch/devintensive/models/TextMessage.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/TextMessage.kt
@@ -3,14 +3,14 @@ package ru.skillbranch.devintensive.models
 import ru.skillbranch.devintensive.extensions.humanizeDiff
 import java.util.*
 
-
-class TextMessage (
-    id: String,
-    from: User?,
-    chat: Chat,
-    isIncoming: Boolean = false,
+class TextMessage(
+    id:String,
+    from:User?,
+    chat:Chat,
+    isIncoming:Boolean = false,
     date: Date = Date(),
-    var text: String?
-) : BaseMessage(id, from, chat, isIncoming, date) {
-    override fun formatMessage(): String = "id:$id ${from?.firstName} ${if(isIncoming) "получила" else "отправила"} сообщение \"$text\" ${date.humanizeDiff()}"
+    var text : String?)
+    : BaseMessage(id, from, chat, isIncoming, date) {
+    override fun formatMessage(): String = "id $id ${from?.firstName} " +
+            "${if (isIncoming) "получил" else "отправил"} сообщение \"$text\" ${date.humanizeDiff()}"
 }
